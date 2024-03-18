9d5ecb5be532dd2b277b73f452bdf141a48bf24c
Zouhair Ouddach <zouddach@e3r3p6.1337.ma>
2024-03-18 03:06:54 +0000

    done

--- SRC/push_functions.c
+++ SRC/push_functions.c
@@ -6,16 +6,16 @@
 /*   By: zouddach <zouddach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 03:00:11 by zouddach          #+#    #+#             */
-/*   Updated: 2024/03/18 03:04:11 by zouddach         ###   ########.fr       */
+/*   Updated: 2024/03/18 03:06:51 by zouddach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_push(t_stack *from, t_stack *to, int code)
+void	ft_push(t_stack *from, t_stack *to, int code)//from == b and to ==a hia push a || pa
 {
 	//khasni npushi mn from l to gher ra9m lwl
-	if (code == 1)
+	if (from->id == 1)
 		write(1, "pa\n", 3);
 	else
 		write(1, "pb\n", 3);
