fdf5374730200ade9a7406e6473abd0bfbe9d5c7
Zouhair Ouddach <zouddach@e3r3p6.1337.ma>
2024-03-18 03:03:53 +0000

    done

--- SRC/push_functions.c
+++ SRC/push_functions.c
@@ -6,7 +6,7 @@
 /*   By: zouddach <zouddach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 03:00:11 by zouddach          #+#    #+#             */
-/*   Updated: 2024/03/18 03:01:33 by zouddach         ###   ########.fr       */
+/*   Updated: 2024/03/18 03:03:43 by zouddach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,7 @@
 
 void	ft_push(t_stack *from, t_stack *to, int code)
 {
-	int	i;
-
-	if (from->size == 0)
-		return ;
-	i = to->size;
-	while (i > 0)
-	{
-		to->numbers[i] = to->numbers[i - 1];
-		i--;
-	}
-	to->numbers[0] = from->numbers[0];
-	i = 0;
-	while (i < from->size - 1)
-	{
-		from->numbers[i] = from->numbers[i + 1];
-		i++;
-	}
-	from->size--;
-	to->size++;
+	//khasni npushoi mn from l to gher ra9m lwl
 	if (code == 1)
 		write(1, "pa\n", 3);
 	else
